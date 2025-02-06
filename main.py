884bff101adada0472bf5b5930aaa9264ed80a8d
Lucy.Ingerman <lucy.ingerman@gmail.com>
2025-02-06 12:40:20 +0100

    meth

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+
+
+def project_point(P, u=None, v=None, n=None):
+    if n is None:
+        n = np.cross(u, v)
+    projection = P - (np.dot(P, n) / (np.linalg.norm(n)**2)) * n
+    return projection
+
+
+vector_u = np.array([1, 1, 0])
+vector_V = np.array([0, -1, 0])
+P = np.array([math.pi, math.e, 1])
+print(project_point(P, vector_u, vector_V))
+print(project_point(P, vector_u, -vector_V))
+
+
+n = np.array([1, 1, 1])
+m = np.array([3, 3, 3])
+print(project_point(P, n=n))
+print(project_point(P, n=m))
