fe1221875c79d23b7854b2e226c68e48e48524b1
Lucy.Ingerman <lucy.ingerman@gmail.com>
2025-02-06 12:48:20 +0100

    le comments

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ def project_point(P, u=None, v=None, n=None):
 vector_u = np.array([1, 1, 0])
 vector_V = np.array([0, -1, 0])
 P = np.array([math.pi, math.e, 1])
+
+
+# Resultaten blir identiska då den negativa vektorn av v fortfarande spänner upp samma plan
+# skillnaden blir då att punkter och vektorer förändras, men i vårt fall använder vi normalvektorn 
+# därmerd kommer projektionen vara likadan
 print(project_point(P, vector_u, vector_V))
 print(project_point(P, vector_u, -vector_V))
 
 
 n = np.array([1, 1, 1])
 m = np.array([3, 3, 3])
+# Resultaten blir identiska då båda vektorerna är linjärt beroende och därmed är normalvektorer för samma plan
 print(project_point(P, n=n))
 print(project_point(P, n=m))
